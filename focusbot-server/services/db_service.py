c24101ca47c0cd825d0114e20ad336d83c057425
guille15j <guillermosantosanchez@gmail.com>
2026-03-27 19:01:37 +0100

    Mejora de la legibilidad y correccion de errores

--- focusbot-server/services/db_service.py
+++ focusbot-server/services/db_service.py
@@ -3,43 +3,7 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-# Definicion de los modelos de tablas
-
-# Para que Flask entienda la clase como tabla debe heredar de dn.Model
-class User(db.Model):
-    __tablename__ = 'users'
-    
-    user_id = db.Column (db.Integer, primary_key = True)
-    name_usr = db.Column (db.String(50), nullable=False )
-    srname_usr = db.Column (db.String(50), nullable=False  )
-    nickname = db.Column (db.String(20), nullable=False  )
-    phone = db.Column (db.Integer )
-    mail = db.Column (db.String(100), nullable=False  )
-    hash_pwsd = db.Column (db.Text, nullable=False  )
-    bth_date = db.Column (db.Date, nullable=False  )
-    img = db.Column (db.Text )
-    timezone = db.Column(db.String(50), default='UTC')
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-class ActivityType (db.Model):
-    __tablename__ = "activity_types"
-
-    type_id = db.Column(db.Integer, primary_key= True)
-    total_time = db.Column(db.Integer, nullable = False)
-    rest_time = db.Column(db.Integer, default = 0)
-    break_time = db.Column(db.Integer, default = 0)
-    num_breaks = db.Column(db.Integer, default = 0)
-    name_type = db.Column(db.String(50), nullable =False)
-
-    # Constraints
-    __table_args__ = (
-        db.CheckConstraint('rest_time < total_time', name='check_rest_less_than_total'),
-        db.CheckConstraint('total_time >= 0', name='check_total_positive'),
-        db.CheckConstraint('rest_time >= 0', name='check_rest_positive'),
-        db.CheckConstraint('break_time >= 0', name='check_break_positive'),
-        db.CheckConstraint('num_breaks >= 0', name='check_num_breaks_positive'),
-    )
-
+# ENUMERADORES
 class BotStatus(enum.Enum):
     OFFLINE = "OFFLINE"
     BOOTING = "BOOTING"
@@ -51,26 +15,12 @@ class BotStatus(enum.Enum):
     FINISHED = "FINISHED"
     ERROR = "ERROR"
 
-
-class Bot(db.Model):
-    __tablename__ = 'bots'
-
-    bot_id = db.Column(db.Integer, primary_key = True)
-    mac_ddr = db.Column(db.String(17), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = True) # No es nullable porque si que peude existir un robot al que aun no se le ha asignado nignun usuario
-    custom_name = db.Column(db.String(20), default = 'Focus-Bot') 
-    passKey = db.Column(db.Text, nullable = False) # Passwd para identificarse durante la comunicación
-    access_point = db.Column(db.String(150), nullable = False) # SSID generado por BOT
-    last_sync = db.Column(db.DateTime)
-    status = db.Column(sb.String(20), nullable=False, default=BotStatus.OFFLINE)
-    finware_version = db.Column(db.String(20),nullable = True)
-
 class ActivityState(enum.Enum):
     PENDIENTE = 'PENDIENTE'
     POSPUESTO = 'POSPUESTO'
     COMPLETADO = 'COMPLETADO'
     CANCELADO = 'CANCELADO'
-    CURSO = 'EN CURSO'
+    EN_CURSO = 'EN CURSO'
 
 class ActivityCategory (enum.Enum):
     ESTUDIOS = 'ESTUDIOS'
@@ -80,10 +30,64 @@ class ActivityCategory (enum.Enum):
     DESCANSO = 'DESCANSO'
     OTRAS = 'OTRAS'
 
-class ActivityResults (enum.Enum):
-    COMPLETADO = 'COMPLETADO'
-    PARCIAL = 'PARCIAL'
-    FALLADO = 'FALLADO'
+class ServerityEnum (enum.Enum):
+    LEVE = 'LEVE'
+    MEDIO = 'MEDIO'
+    ALTO = 'ALTO'
+
+# Definicion de los modelos de tablas
+
+# Para que Flask entienda la clase como tabla debe heredar de dn.Model
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    user_id = db.Column (db.Integer, primary_key = True)
+    name_usr = db.Column (db.String(50), nullable=False )
+    srname_usr = db.Column (db.String(50), nullable=False  )
+    nickname = db.Column (db.String(20), nullable=False ,unique=True )
+    phone = db.Column(db.String(20))
+    mail = db.Column (db.String(100), nullable=False, unique=True)
+    hash_pwsd = db.Column (db.Text, nullable=False  )
+    bth_date = db.Column (db.Date, nullable=False  )
+    img = db.Column (db.Text )
+    timezone = db.Column(db.String(50), default='UTC')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    activities = db.relationship('Activity', backref='user', lazy=True)
+    bots = db.relationship('Bot', backref='owner', lazy=True)
+    details = db.relationship('Detail', backref='user', lazy=True)
+
+class ActivityType (db.Model):
+    __tablename__ = "activity_types"
+
+    type_id = db.Column(db.Integer, primary_key= True)
+    name_type = db.Column(db.String(50), nullable =False)
+    total_time = db.Column(db.Integer, nullable = False)
+    rest_time = db.Column(db.Integer, default = 0)
+    break_time = db.Column(db.Integer, default = 0)
+    num_breaks = db.Column(db.Integer, default = 0)
+
+    # Constraints
+    __table_args__ = (
+        db.CheckConstraint('rest_time < total_time', name='check_rest_less_than_total'),
+        db.CheckConstraint('total_time >= 0', name='check_total_positive'),
+        db.CheckConstraint('rest_time >= 0', name='check_rest_positive'),
+        db.CheckConstraint('break_time >= 0', name='check_break_positive'),
+        db.CheckConstraint('num_breaks >= 0', name='check_num_breaks_positive'),
+    )
+
+class Bot(db.Model):
+    __tablename__ = 'bots'
+
+    bot_id = db.Column(db.Integer, primary_key=True)
+    mac_address = db.Column(db.String(17), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+    custom_name = db.Column(db.String(50), default='Focus-Bot')
+    pass_key = db.Column(db.Text, nullable=False)
+    access_point_ssid = db.Column(db.String(150), nullable=False)
+    last_sync = db.Column(db.DateTime)
+    status = db.Column(db.Enum(BotStatus), nullable=False, default=BotStatus.OFFLINE)
+    firmware_version = db.Column(db.String(20))
 
 class Activity(db.Model):
     __tablename__ = 'activities'
@@ -92,34 +96,31 @@ class Activity(db.Model):
     type_id = db.Column(db.Integer, db.ForeignKey('activity_types.type_id'), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
     bot_id = db.Column(db.Integer, db.ForeignKey('bots.bot_id'), nullable = False)
+
     title = db.Column (db.String(50), nullable = False)
     description = db.Column (db.String(250))
     init_date = db.Column(db.DateTime)
     end_date = db.Column(db.DateTime)
-    state = db.Column(db.String(20), nullable = False, default = ActivityState.PENDIENTE )
-    category = db.Column(db.String(20), nullable = False, default = ActivityCategory.ESTUDIOS)
-    result = db.Column(db.String(20), nullable = True)
+    
+    state = db.Column(db.Enum(ActivityState), nullable=False, default=ActivityState.PENDIENTE)
+    category = db.Column(db.Enum(ActivityCategory), nullable=False, default=ActivityCategory.OTRAS)
 
-class ServerityEnum (enum.Enum):
-    LEVE = 'LEVE'
-    MEDIO = 'MEDIO'
-    ALTO = 'ALTO'
+    result = db.Column(db.Enum(ActivityResults), nullable=True)
 
 class Details(db.Model):
     __tablename__ = 'details'
 
     # Clave compuesta
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key = True)
-    name_detail = db.Column(db.String(10), primary_key = True)
+    name_detail = db.Column(db.String(50), primary_key = True)
     description_detail = db.Column(db.String(250))
-    serverity = db.Column(db.String(5), default = ServerityEnum.LEVE)
-
+    severity = db.Column(db.Enum(SeverityEnum), default=SeverityEnum.LEVE)
 
 class History(db.Model):
     __tablename__ = 'histories'
 
     result_id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
 
     first_date_range = db.Column(db.DateTime, nullable = False)
     end_date_range = db.Column(db.DateTime, nullable = False)
@@ -130,8 +131,7 @@ class History(db.Model):
     num_pendiente = db.Column(db.Integer, default=0)
 
     avg_focus = db.Column(db.Float, default=0.0)
-    num_activities = db.Column(db.Integer, default=0)
-
+    total_activities = db.Column(db.Integer, default=0)
     total_used_time = db.Column(db.Interval)
 
     __table_args__ = (
