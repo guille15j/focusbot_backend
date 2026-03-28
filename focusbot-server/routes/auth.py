86e98ba84a0943a969a7d998396df94b5dddde83
guille15j <guillermosantosanchez@gmail.com>
2026-03-28 13:17:22 +0100

    Configuracion del enpoint de registrod e usuario

--- focusbot-server/routes/auth.py
+++ focusbot-server/routes/auth.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, jsonify, request
+from services.auth_service import register_user
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth', __name__) #Nos permite agripar los endpoints en bloques sin que tengan que estar todos en un mismo archivo
 
-@auth_bp.route('/login', methods=['POST'])
-def login():
+@auth_bp.route('/authcheck', methods=['POST'])
+def check():
     return jsonify({"message": "Endpoint de autenticación listo"}), 200
+
+@auth_bp.route('/register',methods=['POST'])
+def register():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'error':'Datos vacios en la petición'}),400
+    
+    response, status_code = register_user(data)
+
+    retrun jsonify(response), status_code
