ee207a3537243d041563bb3eecbf82b79d6b2179
guille15j <guillermosantosanchez@gmail.com>
2026-03-28 23:01:28 +0100

    Preparación de endpoints de actividades

--- focusbot-server/routes/activities.py
+++ focusbot-server/routes/activities.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, jsonify
+from utils import *
 
 activities_bp = Blueprint('activities', __name__)
 
-@activities_bp.route('/', methods=['GET'])
+@activities_bp.route('/check', methods=['GET'])
 def get_activities():
-    return jsonify({"activities": [], "message": "Listado de actividades"}), 200
+    return jsonify({"message": "API preparada para recibir peticiones"}), 200
+
+@activities_bp.route('/user/focus-settings', methods=['GET'])
+@token_required
+def focusSettings(current_user):
+    return None
+
+@activities_bp.route('/user/focus-settings',methods=['POST'])
+@token_required
+def setFocusSettings(current_user):
+    return None
+
+@activities_bp.route('/user/focus-settings', methods=['PATCH'])
+@token_required
+def updateFocusSettings(current_user):
+    return None
