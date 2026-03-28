f30ee130084105821542528013a39a38eb9adcb2
guille15j <guillermosantosanchez@gmail.com>
2026-03-28 23:15:45 +0100

    Creacion endpoints de historial

--- focusbot-server/routes/history.py
+++ focusbot-server/routes/history.py
@@ -1,7 +1,18 @@
 from flask import Blueprint, jsonify
+from utils import *
 
 history_bp = Blueprint('history', __name__)
 
-@history_bp.route('/', methods=['GET'])
+@history_bp.route('/check', methods=['GET'])
 def get_history():
-    return jsonify({"history": [], "message": "Historial de enfoque"}), 200
+    return jsonify({"message": "API de historial lista para recibir peticones"}), 200
+
+@history_bp.route('/', methods=['GET'])
+@token_required
+def getHistorial(current_user):
+    return None
+
+@history_bp.route('/stats/summary', methods=['GET'])
+@token_required
+def getSumarioHistorial(current_user):
+    return None
