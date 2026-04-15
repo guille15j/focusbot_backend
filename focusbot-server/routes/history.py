8123dc0564416a9485ad5d7f78d8be3aacd3ff2b
guille <practicas006@roadtodata>
2026-04-15 10:53:45 +0200

    Creación del archivo history_service.py

--- focusbot-server/routes/history.py
+++ focusbot-server/routes/history.py
@@ -7,12 +7,3 @@ history_bp = Blueprint('history', __name__)
 def get_history():
     return jsonify({"message": "API de historial lista para recibir peticones"}), 200
 
-@history_bp.route('/', methods=['GET'])
-@token_required
-def getHistorial(current_user):
-    return None
-
-@history_bp.route('/stats/summary', methods=['GET'])
-@token_required
-def getSumarioHistorial(current_user):
-    return None
