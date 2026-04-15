c7dc1ef5ea03bcb3425e83b27ed8e8fe0159d1f1
guille <practicas006@roadtodata>
2026-04-15 10:01:53 +0200

    Actualización de fichero init de services para incluir los servicios totales descritos en los ficheros

--- focusbot-server/services/__init__.py
+++ focusbot-server/services/__init__.py
@@ -1,16 +1,24 @@
-from .db_service import db, User, Bot, Activity, ActivityState, BotStatus
+from .db_service import db, User, Bot, Activity, ActivityState, BotStatus, ActivityType, Detail, History
 from .auth_service import register_user, login_user, reset_password
-from .mqtt_service import init_mqtt, mqtt_client, asegurar_conexion
-from .bot_service import link_bot, getBotsByUser
+from .mqtt_service import init_mqtt, mqtt_client, on_connect, asegurar_conexion
+from .bot_service import link_bot, getBotsByUser, getBotById, editBot, deleteBot
 from .user_service import updateUserPatch, getUser, getDetail, updateDetail, createDetail
-from .activity_service import getActivitiesUsr
+from .activity_service import getActivitiesUsr, getActivity, createActivity, editActivity, deleteActivity, getTypesUsr, createType, editType, deleteType
 
 __all__ = [
     # BBDD
     'db', 
     'User', 
     'Bot',
-    'Activity', 
+    'Activity',
+    'ActivityType',
+    'Detail',
+    'History',
+    'BotStatus',
+    'ActivityState',
+    'ActivityCategory',
+    'SeverityEnum',
+    'ActivityResults',
 
     # Authentication
     'register_user', 
@@ -18,8 +26,10 @@ __all__ = [
     'reset_password',
 
     # MQTT
+    'mqtt_client',
     'init_mqtt',
     'asegurar_conexion',
+    'on_connect',
 
     # User
     'updateUserPatch',
@@ -31,7 +41,18 @@ __all__ = [
     # Bots
     'link_bot',
     'getBotsByUser',
+    'getBotById', 
+    'editBot', 
+    'deleteBot',
 
     # Activities
-    'getActivitiesUsr'
+    'getActivitiesUsr',
+    'getActivity', 
+    'createActivity', 
+    'editActivity', 
+    'deleteActivity', 
+    'getTypesUsr', 
+    'createType', 
+    'editType', 
+    'deleteType'
 ]
